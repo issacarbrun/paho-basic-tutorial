71b6409e46176ab461d7e20a061d1541e9deb6ec
isbrunow <issacarbrunow@gmail.com>
2023-12-03 09:27:06 -0300

    empapp teste

--- App.js
+++ App.js
@@ -1,10 +1,47 @@
+import Paho from "paho-mqtt";
+import { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const options = {
+  host: 'logix.useletric.com.br',
+  port: 1883,
+  path: 'weather/app',
+  id: 'id_' + parseInt(Math.random()*100000)
+}
+
+const client = new Paho.Client(
+  "logix.useletric.com.br",
+  Number(1883),
+  `${parseInt(Math.random()*100)}`
+);
+
+//client = new Paho.Client(options.host, options.port, options.path);
 export default function App() {
+
+  const [value, setValue] = useState(0);
+
+  function onMessage(message){
+    if(message.destinationName === "weather/app")
+      setValue(parseInt(message.payloadString));
+  }
+
+  useEffect(() =>{
+    client.connect({
+      onSuccess: () => {
+        console.log("Connected!");
+        client.subscribe("weather/app");
+        client.onMessageArrived = onMessage;
+      },
+      onFailure: () =>{
+        console.log("Failed to connected!")
+      }
+    });
+  },[])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Value is: {value}</Text>
       <StatusBar style="auto" />
     </View>
   );
